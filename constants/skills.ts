a48af872179915665623e4f631e0c9aed5fd9368
Matt Huggins <matt.huggins@gmail.com>
2023-09-11 19:07:00 -0500

    Update skills

--- constants/skills.ts
+++ constants/skills.ts
@@ -1,8 +1,8 @@
 export const SKILLS = [
-  { name: 'React', years: 8, proficiency: 'Expert' },
-  { name: 'TypeScript', years: 3, proficiency: 'Advanced' },
-  { name: 'JavaScript', years: 17, proficiency: 'Expert' },
-  { name: 'CSS / SCSS', years: 17, proficiency: 'Expert' },
+  { name: 'React', years: 9, proficiency: 'Expert' },
+  { name: 'TypeScript', years: 4, proficiency: 'Advanced' },
+  { name: 'JavaScript', years: 18, proficiency: 'Expert' },
+  { name: 'CSS / SCSS', years: 18, proficiency: 'Expert' },
   { name: 'Ruby / Rails', years: 10, proficiency: 'Expert' },
   { name: 'SQL', years: 10, proficiency: 'Advanced' },
   { name: 'Java', years: 5, proficiency: 'Moderate' },
