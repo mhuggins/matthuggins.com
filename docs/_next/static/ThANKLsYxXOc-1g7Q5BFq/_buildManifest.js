be3002d7822041fe7103b04c7d5d5dcc878969f4
Matt Huggins <matt.huggins@gmail.com>
2023-09-11 19:07:18 -0500

    Publish changes

---
+++ docs/_next/static/ThANKLsYxXOc-1g7Q5BFq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7482bcd2e8d5fdf6.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/contact":["static/chunks/pages/contact-989d6e804ba12394.js"],sortedPages:["/","/_app","/_error","/contact"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
