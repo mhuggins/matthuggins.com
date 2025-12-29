85ef9a49a66430b4c5f37d8d02362fd2f5b91c1f
Matt Huggins <matt.huggins@gmail.com>
2025-12-29 15:26:48 -0600

    fix date discrepancy between server & client

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,7 +2,12 @@ export function formatDate(str: string) {
   const date = new Date(str);
 
   if (!Number.isNaN(date.getTime())) {
-    return date.toLocaleDateString(undefined, { year: "numeric", month: "short", day: "numeric" });
+    return date.toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      timeZone: "America/Chicago",
+    });
   }
 
   return str;
